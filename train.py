b49e7806657bdee0c35e0f8c01df9a48cdd37f95
Saulo Medeiros de Araujo <saulo2@gmail.com>
2020-06-10 09:32:17 -0300

    Update train.py

--- train.py
+++ train.py
@@ -75,12 +75,13 @@ if __name__ == "__main__":
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
-        #if tracking_url_type_store != "file":
+        if tracking_url_type_store != "file":
 
             # Register the model
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             #mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
-        #else:
+            mlflow.sklearn.log_model(lr, "model")
+        else:
             mlflow.sklearn.log_model(lr, "model")
